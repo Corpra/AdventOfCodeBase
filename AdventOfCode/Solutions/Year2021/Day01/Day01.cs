1a3bd205a03f9cf4b6a773c6883698b76a895fb3
Mathias Jernberg <mathias.jernberg@nuway.se>
2021-12-02 17:07:58 +0100

    Added correct solutions for Day 1

---
+++ AdventOfCode/Solutions/Year2021/Day01/Day01.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2021
+{
+
+	class Day01 : ASolution
+	{
+
+		public Day01() : base(01, 2021, "")
+		{
+
+		}
+
+		protected override string SolvePartOne()
+		{
+			var result = 0;
+			var prevLineVal = 100000;
+
+			foreach (var line in System.IO.File.ReadLines("input.txt"))
+			{
+				var lineVal = Convert.ToInt32(line);
+				if (prevLineVal < lineVal)
+					result++;
+
+				prevLineVal = lineVal;
+			}
+
+			Console.WriteLine("The answer is {0}.", result);
+
+			return result.ToString();
+		}
+
+		protected override string SolvePartTwo()
+		{
+			var result = 0;
+			var index = 0;
+
+			var setA = new List<int>();
+			var setB = new List<int>();
+			var setC = new List<int>();
+
+			var prevSetValA = 0;
+			var prevSetValB = 0;
+			var prevSetValC = 0;
+
+			foreach (var line in System.IO.File.ReadLines("input.txt"))
+			{
+				var lineVal = Convert.ToInt32(line);
+
+				setA.Add(lineVal);
+
+				if (setA.Count == 3)
+				{
+					if (prevSetValC > 0 && setA.Sum() > prevSetValC)
+						result++;
+
+					prevSetValA = setA.Sum();
+					setA.Clear();
+				}
+
+				if (index > 0)
+					setB.Add(lineVal);
+				
+				if (setB.Count == 3)
+				{
+					if (prevSetValA > 0 && setB.Sum() > prevSetValA)
+						result++;
+
+					prevSetValB = setB.Sum();
+					setB.Clear();
+				}
+
+				if (index > 1)
+					setC.Add(lineVal);
+
+				if (setC.Count == 3)
+				{
+					if (prevSetValB > 0 && setC.Sum() > prevSetValB)
+						result++;
+
+					prevSetValC = setC.Sum();
+					setC.Clear();
+				}
+
+				index++;
+			}
+
+			Console.WriteLine("The answer is {0}.", result);
+
+			return result.ToString();
+		}
+	}
+}
