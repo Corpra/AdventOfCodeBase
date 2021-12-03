16578fd92439990ddf042b2670a234f0b14561d5
Mathias Jernberg <mathias.jernberg@nuway.se>
2021-12-03 07:35:16 +0100

    Added correct solutions for Day 3

---
+++ AdventOfCode/Solutions/Year2021/Day03/Day03.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Linq;
+
+namespace AdventOfCode.Solutions.Year2021
+{
+
+	class Day03 : ASolution
+	{
+
+		public Day03() : base(03, 2021, "Binary Diagnostic")
+		{
+
+		}
+
+		protected override string SolvePartOne()
+		{
+			var gammaRate = "";
+			var epsilonRate = "";
+
+			var inputs = System.IO.File.ReadLines("inputs/Day03.txt").ToList();
+			var inputLength = inputs.First().Length;
+
+			for (var i = 0; i < inputLength; i++)
+			{
+				var numberOfZeros = inputs.Count(x => x[i] == '0');
+				var numberOfOnes = inputs.Count(x => x[i] == '1');
+
+				gammaRate += numberOfZeros > numberOfOnes ? "0" : "1";
+				epsilonRate += numberOfZeros > numberOfOnes ? "1" : "0";
+			}
+
+			var result = Convert.ToInt32(gammaRate, 2) * Convert.ToInt32(epsilonRate, 2);
+
+			return result.ToString();
+		}
+
+		protected override string SolvePartTwo()
+		{
+			var oxygenRating = "";
+			var co2Rating = "";
+
+			var inputs = System.IO.File.ReadLines("inputs/Day03.txt").ToList();
+			var inputLength = inputs.First().Length;
+
+			for (var i = 0; i < inputLength; i++)
+			{
+				var inputsForOxygenRating = inputs.Where(x => x.StartsWith(oxygenRating));
+
+				if (inputsForOxygenRating.Count() > 1)
+				{
+					var numberOfZeros = inputsForOxygenRating.Count(x => x[i] == '0');
+					var numberOfOnes = inputsForOxygenRating.Count(x => x[i] == '1');
+
+					oxygenRating += numberOfZeros > numberOfOnes ? "0" : "1";
+				}
+				else
+				{
+					oxygenRating = inputsForOxygenRating.First();
+				}
+
+				var inputsForCo2Rating = inputs.Where(x => x.StartsWith(co2Rating));
+
+				if (inputsForCo2Rating.Count() > 1)
+				{
+					var numberOfZeros = inputsForCo2Rating.Count(x => x[i] == '0');
+					var numberOfOnes = inputsForCo2Rating.Count(x => x[i] == '1');
+
+					co2Rating += numberOfZeros <= numberOfOnes ? "0" : "1";
+				}
+				else
+				{
+					co2Rating = inputsForCo2Rating.First();
+				}
+			}
+
+			var result = Convert.ToInt32(oxygenRating, 2) * Convert.ToInt32(co2Rating, 2);
+
+			return result.ToString();
+		}
+	}
+}
