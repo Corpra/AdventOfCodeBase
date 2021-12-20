d3bdea0dcd8a65a6f63a3e2bbe44018db4ee26b8
Mathias Jernberg <mathias.jernberg@nuway.se>
2021-12-20 15:59:00 +0100

    Added solutions for Day 20

--- AdventOfCode/Solutions/Year2021/Day20/Day20.cs
+++ AdventOfCode/Solutions/Year2021/Day20/Day20.cs
@@ -1,5 +1,4 @@
 //using System;
-//using System.Collections.Generic;
 //using System.Linq;
 
 //namespace AdventOfCode.Solutions.Year2021
@@ -8,19 +7,168 @@
 //	class Day20 : ASolution
 //	{
 
-//		public Day20() : base(20, 2021, "")
+//		public Day20() : base(20, 2021, "Trench Map")
 //		{
 
 //		}
 
 //		protected override string SolvePartOne()
 //		{
-//			return null;
+//			var inputLines = System.IO.File.ReadLines("C:/Projects/Advent of code/AdventOfCodeBase/AdventOfCode/inputs/Day20.txt").ToList();
+
+//			var algorithm = inputLines[0];
+
+//			var gridXSize = inputLines[2].Length;
+//			var gridYSize = inputLines.Count - 2;
+//			var grid = new char[gridXSize * 3, gridYSize * 3];
+//			var yy = gridYSize;
+//			foreach (var line in inputLines.Skip(2))
+//			{
+//				var x = gridXSize;
+//				foreach (var position in line)
+//				{
+//					grid[x, yy] = position;
+//					x++;
+//				}
+//				yy++;
+//			}
+
+//			for (int y = 0; y < gridYSize * 3; y++)
+//			{
+//				for (int x = 0; x < gridXSize * 3; x++)
+//				{
+//					if (grid[x, y] != '.' && grid[x, y] != '#')
+//						grid[x, y] = '.';
+//				}
+//			}
+
+//			for (int l = 0; l < 2; l++)
+//			{
+//				var newGrid = new char[gridXSize * 3, gridYSize * 3];
+
+//				for (int y = 0; y < gridYSize * 3; y++)
+//				{
+//					for (int x = 0; x < gridXSize * 3; x++)
+//					{
+//						newGrid[x, y] = '.';
+//					}
+//				}
+
+//				for (int y = 0; y < gridYSize * 3; y++)
+//				{
+//					for (int x = 0; x < gridXSize * 3; x++)
+//					{
+//						var x11 = x == 0 ? (y == 0 ? grid[x, y] : grid[x, y - 1]) : (y == 0 ? grid[x - 1, y] : grid[x - 1, y - 1]);
+//						var x12 = y == 0 ? grid[x, y] : grid[x, y - 1];
+//						var x13 = y == 0 ? (x == (gridXSize * 3 - 1) ? grid[x, y] : grid[x + 1, y]) : (x == (gridXSize * 3 - 1) ? grid[x, y - 1] : grid[x + 1, y - 1]);
+//						var x21 = x == 0 ? grid[x, y] : grid[x - 1, y];
+//						var x22 = grid[x, y];
+//						var x23 = x == (gridXSize * 3 - 1) ? grid[x, y] : grid[x + 1, y];
+//						var x31 = x == 0 ? (y == (gridYSize * 3 - 1) ? grid[x, y] : grid[x, y + 1]) : (y == (gridYSize * 3 - 1) ? grid[x - 1, y] : grid[x - 1, y + 1]);
+//						var x32 = y == gridYSize * 3 - 1 ? grid[x, y] : grid[x, y + 1];
+//						var x33 = y == gridYSize * 3 - 1 ? (x == (gridXSize * 3 - 1) ? grid[x, y] : grid[x + 1, y]) : (x == (gridXSize * 3 - 1) ? grid[x, y + 1] : grid[x + 1, y + 1]);
+//						var binary = $"{GetNumber(x11)}{GetNumber(x12)}{GetNumber(x13)}{GetNumber(x21)}{GetNumber(x22)}{GetNumber(x23)}{GetNumber(x31)}{GetNumber(x32)}{GetNumber(x33)}";
+
+//						var index = Convert.ToInt32(binary, 2);
+
+//						newGrid[x, y] = algorithm[index];
+//					}
+//				}
+
+//				grid = newGrid;
+//			}
+
+//			var result = 0;
+//			foreach (var c in grid)
+//			{
+//				if (c == '#')
+//					result++;
+//			}
+
+//			return result.ToString();
 //		}
 
 //		protected override string SolvePartTwo()
 //		{
-//			return null;
+//			var inputLines = System.IO.File.ReadLines("C:/Projects/Advent of code/AdventOfCodeBase/AdventOfCode/inputs/Day20.txt").ToList();
+
+//			var algorithm = inputLines[0];
+
+//			var gridXSize = inputLines[2].Length;
+//			var gridYSize = inputLines.Count - 2;
+//			var grid = new char[gridXSize * 3, gridYSize * 3];
+//			var yy = gridYSize;
+//			foreach (var line in inputLines.Skip(2))
+//			{
+//				var x = gridXSize;
+//				foreach (var position in line)
+//				{
+//					grid[x, yy] = position;
+//					x++;
+//				}
+//				yy++;
+//			}
+
+//			for (int y = 0; y < gridYSize * 3; y++)
+//			{
+//				for (int x = 0; x < gridXSize * 3; x++)
+//				{
+//					if (grid[x, y] != '.' && grid[x, y] != '#')
+//						grid[x, y] = '.';
+//				}
+//			}
+
+//			for (int l = 0; l < 50; l++)
+//			{
+//				var newGrid = new char[gridXSize * 3, gridYSize * 3];
+
+//				for (int y = 0; y < gridYSize * 3; y++)
+//				{
+//					for (int x = 0; x < gridXSize * 3; x++)
+//					{
+//						newGrid[x, y] = '.';
+//					}
+//				}
+
+//				for (int y = 0; y < gridYSize * 3; y++)
+//				{
+//					for (int x = 0; x < gridXSize * 3; x++)
+//					{
+//						var x11 = x == 0 ? (y == 0 ? grid[x, y] : grid[x, y - 1]) : (y == 0 ? grid[x - 1, y] : grid[x - 1, y - 1]);
+//						var x12 = y == 0 ? grid[x, y] : grid[x, y - 1];
+//						var x13 = y == 0 ? (x == (gridXSize * 3 - 1) ? grid[x, y] : grid[x + 1, y]) : (x == (gridXSize * 3 - 1) ? grid[x, y - 1] : grid[x + 1, y - 1]);
+//						var x21 = x == 0 ? grid[x, y] : grid[x - 1, y];
+//						var x22 = grid[x, y];
+//						var x23 = x == (gridXSize * 3 - 1) ? grid[x, y] : grid[x + 1, y];
+//						var x31 = x == 0 ? (y == (gridYSize * 3 - 1) ? grid[x, y] : grid[x, y + 1]) : (y == (gridYSize * 3 - 1) ? grid[x - 1, y] : grid[x - 1, y + 1]);
+//						var x32 = y == gridYSize * 3 - 1 ? grid[x, y] : grid[x, y + 1];
+//						var x33 = y == gridYSize * 3 - 1 ? (x == (gridXSize * 3 - 1) ? grid[x, y] : grid[x + 1, y]) : (x == (gridXSize * 3 - 1) ? grid[x, y + 1] : grid[x + 1, y + 1]);
+//						var binary = $"{GetNumber(x11)}{GetNumber(x12)}{GetNumber(x13)}{GetNumber(x21)}{GetNumber(x22)}{GetNumber(x23)}{GetNumber(x31)}{GetNumber(x32)}{GetNumber(x33)}";
+
+//						var index = Convert.ToInt32(binary, 2);
+
+//						newGrid[x, y] = algorithm[index];
+//					}
+//				}
+
+//				grid = newGrid;
+//			}
+
+//			var result = 0;
+//			foreach (var c in grid)
+//			{
+//				if (c == '#')
+//					result++;
+//			}
+
+//			return result.ToString();
+//		}
+
+//		private string GetNumber(char sign)
+//		{
+//			if (sign == '.')
+//				return "0";
+//			return "1";
 //		}
 //	}
 //}
